ced9d63af40e506ed155c2b8860d87ea974f6dfe
khchehab <khchehab@gmail.com>
2021-09-19 18:45:02 +0300

    Removed setting frame for stack of analog clock to have it centered and depend on the geometry proxy.

--- Clocksy/Views/AnalogClockView.swift
+++ Clocksy/Views/AnalogClockView.swift
@@ -21,7 +21,6 @@ struct AnalogClockView: View {
                 let (hours, minutes, seconds) = getComponents(of: now)
                 ArmsView(hours: hours, minutes: minutes, seconds: seconds)
             }
-            .frame(width: proxy.circle.width, height: proxy.circle.height)
         }
     }
 }
